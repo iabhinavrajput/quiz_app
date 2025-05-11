a84d19f996e0d1b181711ac234a21b0ab500e0f2
iabhinavrajput <abhinavrajput207@gmail.com>
2025-05-11 23:56:22 +0530

    chnages in the user history model

--- lib/data/models/user_history_model.dart
+++ lib/data/models/user_history_model.dart
@@ -1,24 +1,27 @@
+
 class UserHistory {
   final int score;
   final String difficulty;
-  final List<String> answers;
+  final List<Map<String, dynamic>> answers; 
 
-  UserHistory({required this.score, required this.difficulty, required this.answers});
+  UserHistory({
+    required this.score,
+    required this.difficulty,
+    required this.answers,
+  });
 
-  Map<String, dynamic> toJson() {
+  Map<String, dynamic> toMap() {
     return {
       'score': score,
       'difficulty': difficulty,
-      'answers': answers,
+      'answers': answers.map((answer) {
+        return {
+          'question': answer['question'],
+          'selectedAnswer': answer['selectedAnswer'],
+          'correctAnswer': answer['correctAnswer'], 
+          'isCorrect': answer['isCorrect'],
+        };
+      }).toList(),
     };
   }
-
-  factory UserHistory.fromJson(Map<String, dynamic> json) {
-    return UserHistory(
-      score: json['score'],
-      difficulty: json['difficulty'],
-      answers: List<String>.from(json['answers']),
-    );
-  }
 }
-
--- lib/data/services/firestore_service.dart
+++ lib/data/services/firestore_service.dart
@@ -26,30 +26,3 @@ class FirestoreService {
   }
 }
 
-
-class UserHistory {
-  final int score;
-  final String difficulty;
-  final List<Map<String, dynamic>> answers; 
-
-  UserHistory({
-    required this.score,
-    required this.difficulty,
-    required this.answers,
-  });
-
-  Map<String, dynamic> toMap() {
-    return {
-      'score': score,
-      'difficulty': difficulty,
-      'answers': answers.map((answer) {
-        return {
-          'question': answer['question'],
-          'selectedAnswer': answer['selectedAnswer'],
-          'correctAnswer': answer['correctAnswer'], 
-          'isCorrect': answer['isCorrect'],
-        };
-      }).toList(),
-    };
-  }
-}
