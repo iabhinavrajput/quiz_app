fd11702c06910ae3cb1c5ce96b28f619c2582e5c
iabhinavrajput <abhinavrajput207@gmail.com>
2025-05-14 10:36:28 +0530

    added the FirebaseAnalyticsObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:no_screenshot/no_screenshot.dart'; // Correct import for no_screenshot
+import 'package:no_screenshot/no_screenshot.dart';
 import 'package:quiz_app/bloc/auth/auth_bloc.dart';
 import 'package:quiz_app/bloc/quiz/quiz_bloc.dart';
 import 'package:quiz_app/data/services/quiz_service.dart';
 import 'package:quiz_app/presentation/screens/auth_screen.dart';
 import 'package:quiz_app/presentation/screens/home_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-   await dotenv.load();
-  NoScreenshot.instance; 
+  await dotenv.load();
+  NoScreenshot.instance;
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -30,26 +32,30 @@ class QuizApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (_) => QuizBloc(QuizService())),
-        BlocProvider(create: (_) => AuthBloc()), // Add AuthBloc here
+        BlocProvider(create: (_) => AuthBloc()),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Quiz App',
+        navigatorObservers: [
+          FirebaseAnalyticsObserver(analytics: analytics),
+        ],
         home: _buildHomeScreen(),
       ),
     );
   }
 
-  // Check if the user is already signed in
   Widget _buildHomeScreen() {
     final user = FirebaseAuth.instance.currentUser;
     if (user != null) {
-      return const HomeScreen(); // If the user is logged in, go to HomeScreen
+      return const HomeScreen();
     } else {
-      return const AuthScreen(); // If the user is not logged in, go to AuthScreen
+      return const AuthScreen();
     }
   }
 }
